c641655fd2352c01eaac18efa4c37d2bdaf265ff
jinkyuhan <gkswlsrb95@gmail.com>
2021-08-20 16:01:35 +0900

    feat: add task 'print' in order to test jenkins

--- fakebook-server/src/main/java/com/jkhan/fakebookserver/auth/AuthController.java
+++ fakebook-server/src/main/java/com/jkhan/fakebookserver/auth/AuthController.java
@@ -19,39 +19,39 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/auth")
 public class AuthController {
 
-  @Autowired
-  private UserService userService;
+    @Autowired
+    private UserService userService;
 
-  @Autowired
-  private JwtProvider jwtProvider;
+    @Autowired
+    private JwtProvider jwtProvider;
 
-  @PostMapping("/login")
-  public CommonResponseBody<AuthTokenBundleDto> login(@RequestBody LoginDto loginInput) {
-    UserAccount loginAttemptUserAccount = userService.getUserByEmail(loginInput.getEmail())
-        .orElseThrow(() -> new InvalidInputException("User with this mail not found", "입력한 이메일로 가입된 계정이 없습니다."));
+    @PostMapping("/login")
+    public CommonResponseBody<AuthTokenBundleDto> login(@RequestBody LoginDto loginInput) {
+        UserAccount loginAttemptUserAccount = userService.getUserByEmail(loginInput.getEmail())
+                .orElseThrow(() -> new InvalidInputException("User with this mail not found", "입력한 이메일로 가입된 계정이 없습니다."));
 
-    if (!loginAttemptUserAccount.validatePassword(loginInput.getPassword())) {
-      throw new InvalidInputException("Password is not matched", "비밀번호가 일치하지 않습니다.");
+        if (!loginAttemptUserAccount.validatePassword(loginInput.getPassword())) {
+            throw new InvalidInputException("Password is not matched", "비밀번호가 일치하지 않습니다.");
+        }
+
+        AuthTokenBundleDto newAccessTokenBundle = jwtProvider.issueNewLoginSession(loginAttemptUserAccount);
+
+        return CommonResponseBody.<AuthTokenBundleDto>builder()
+                .result(ApiResult.SUCCESS)
+                .data(newAccessTokenBundle)
+                .build();
     }
 
-    AuthTokenBundleDto newAccessTokenBundle = jwtProvider.issueNewLoginSession(loginAttemptUserAccount);
-
-    return CommonResponseBody.<AuthTokenBundleDto>builder()
-            .result(ApiResult.SUCCESS)
-            .data(newAccessTokenBundle)
-            .build();
-  }
-
-  // logout
+    // logout
 //  @DeleteMapping("/logout")
 //  public CommonResponseBody<Void> logout(Authentication authentication) {
 //
 //  }
 
-  // refresh
-  @PostMapping("/refresh")
-  public CommonResponseBody<Map<String, AuthTokenDto>> refreshAuth(Authentication authentication) {
-    return CommonResponseBody.<Map<String, AuthTokenDto>>builder().result(ApiResult.SUCCESS).build();
-  }
+    // refresh
+    @PostMapping("/refresh")
+    public CommonResponseBody<Map<String, AuthTokenDto>> refreshAuth(Authentication authentication) {
+        return CommonResponseBody.<Map<String, AuthTokenDto>>builder().result(ApiResult.SUCCESS).build();
+    }
 
 }
